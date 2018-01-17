797080c9c2a49af5035dc3252a54f05752ad81f4
clarkerussell <clarkerussell@clarkes-mbp.dhcp.wustl.edu>
2018-01-17 17:09:24 -0600

    code

---
+++ src/Heartbeat.java
@@ -0,0 +1,22 @@
+
+public class Heartbeat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double sleep =0.0;
+
+		while(true) {
+
+			try {
+				
+				Thread.sleep(1000);
+				sleep += 1;
+				
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(sleep);
+		}
+	}
+}
